20267dfc85a5093a773cce5e3ecc4e0f27112f37
mkryan00 <13073@holbertonstudents.com>
2026-03-16 07:53:30 +0000

    Updated print files

---
+++ print_format.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _print_format - handles format parsing
+ * @format: format string
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
+int _print_format(const char *format, va_list args)
+{
+    int i = 0, count = 0;
+
+    if (!format)
+		return (-1);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;  /* move to the specifier */
+
+			/* Check for dangling '%' at end of string */
+			if (format[i] == '\0')
+				return (-1);
+
+			/* Dispatch to the correct printer */
+			if (format[i] == 'c')
+				count += _print_a_char(args);
+			else if (format[i] == 's')
+				count += _print_a_string(args);
+			else if (format[i] == '%')
+				count += _print_a_percent();
+			else
+			{
+				/* unknown specifier: print literally as %x */
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+
+	return (count);
+}

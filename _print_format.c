a5b70187d6f1b34145e3cda183f3d40103d66c97
mkryan00 <13073@holbertonstudents.com>
2026-03-16 07:51:46 +0000

    Updated Print format

--- _print_format.c
+++ _print_format.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 
-#include "main.h"
-
 /**
  * _print_format - handles format parsing
  * @format: format string
@@ -14,32 +12,41 @@ int _print_format(const char *format, va_list args)
 {
     int i = 0, count = 0;
 
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == 'c')
-                count += _print_a_char(args);
-            else if (format[i] == 's')
-                count += _print_a_string(args);
-            else if (format[i] == '%')
-                count += _print_a_percent();
-            else
-            {
-                /* print unknown specifier as literal %x */
-                write(1, "%", 1);
-                write(1, &format[i], 1);
-                count += 2;
-            }
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        i++;
-    }
+    if (!format)
+		return (-1);
 
-    return (count);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;  /* move to the specifier */
+
+			/* Check for dangling '%' at end of string */
+			if (format[i] == '\0')
+				return (-1);
+
+			/* Dispatch to the correct printer */
+			if (format[i] == 'c')
+				count += _print_a_char(args);
+			else if (format[i] == 's')
+				count += _print_a_string(args);
+			else if (format[i] == '%')
+				count += _print_a_percent();
+			else
+			{
+				/* unknown specifier: print literally as %x */
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+
+	return (count);
 }

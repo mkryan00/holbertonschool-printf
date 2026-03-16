1752b3657a9ce9908aafa9efb19444c7322653d8
mkryan00 <13073@holbertonstudents.com>
2026-03-16 07:22:26 +0000

    Updated header file

--- main.h
+++ main.h
@@ -5,5 +5,10 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+int _print_format(const char *format, va_list args);
+
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_a_percent(void);
 
 #endif
